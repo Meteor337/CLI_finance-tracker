c5aa126b15a119ef36ce6c21a33cd46fb374065e
Meteor337 <kometa1881@gmail.com>
2026-09-12 18:52:56 +0300

    Создана архитектура будущего проекта

---
+++ build/CMakeFiles/cli_finance_tracker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cli_finance_tracker.dir/link.d"
+  "CMakeFiles/cli_finance_tracker.dir/main.cpp.o"
+  "CMakeFiles/cli_finance_tracker.dir/main.cpp.o.d"
+  "cli_finance_tracker"
+  "cli_finance_tracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cli_finance_tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
