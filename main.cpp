7a3eb6a6d6211a6c9edc75cc89ab656cacdba8ed
Meteor337 <kometa1881@gmail.com>
2026-09-21 19:05:46 +0300

    main был полностью написан

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <print>
 #include <sstream>
 #include <string>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -22,78 +21,81 @@ struct Transaction {
     std::string comment; // краткое описание (опционально)
 };
 
-class FileManager {
-public:
-    bool saveToFile(const std::string& filename,
-                    const std::vector<Transaction>& transactions) {
-        std::ofstream file(filename);
+bool saveToFile(const std::string& filename,
+                const std::vector<Transaction>& transactions) {
+    std::ofstream file(filename);
 
-        if (!file.is_open()) {
-            return false;
-        }
-
-        file << "ID;Сумма;Категория;Дата;Тип транзакции (0 = Доход, 1 = "
-                "Расход);Комметарий\n";
-        for (const auto& transaction : transactions) {
-            file << transaction.id << ";" << transaction.amount << ";"
-                 << transaction.category << ";" << transaction.date << ";"
-                 << transaction.type << ";" << transaction.comment << "\n";
-        }
-
-        // проверяем не возникло ли ошибок при записи
-        if (!file) {
-            return false;
-        }
-
-        file.close();
-        return true; // всё прошло успешно
+    if (!file.is_open()) {
+        return false;
     }
 
-    std::vector<Transaction> loadFromFile(const std::string& filename) {
-        std::vector<Transaction> tranactions;
-        std::ifstream file(filename);
-        if (!file.is_open()) {
-            // если файл не открылся, возвращаем пустой вектор
-            return tranactions;
-        }
+    file << "ID;Сумма;Категория;Дата;Тип транзакции (0 = Доход, 1 = "
+            "Расход);Комметарий\n";
+    for (const auto& transaction : transactions) {
+        file << transaction.id << ";" << transaction.amount << ";"
+             << transaction.category << ";" << transaction.date << ";"
+             << transaction.type << ";" << transaction.comment << "\n";
+    }
 
-        std::string line;
-        std::getline(file, line);
+    // проверяем не возникло ли ошибок при записи
+    if (!file) {
+        return false;
+    }
 
-        while (std::getline(file, line)) {
-            if (line.empty()) {
-                continue;
-            }
+    file.close();
+    return true; // всё прошло успешно
+}
 
-            std::stringstream ss(line);
-            std::string id_str, amount_str, category, date, type_str, comment;
-
-            if (std::getline(ss, id_str, ';') &&
-                std::getline(ss, amount_str, ';') &&
-                std::getline(ss, category, ';') &&
-                std::getline(ss, date, ';') &&
-                std::getline(ss, type_str, ';')) {
-
-                std::getline(ss, comment);
-
-                Transaction t;
-                t.id = std::stoi(id_str);
-                t.amount = std::stod(amount_str);
-                t.category = category;
-                t.date = date;
-
-                int type_int = std::stoi(type_str);
-                t.type = static_cast<Type>(type_int);
-
-                t.comment = comment;
-
-                tranactions.push_back(t);
-            }
-        }
-        file.close();
+std::vector<Transaction> loadFromFile(const std::string& filename) {
+    std::vector<Transaction> tranactions;
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        // если файл не открылся, возвращаем пустой вектор
         return tranactions;
     }
-};
+
+    std::string line;
+    std::getline(file, line);
+
+    while (std::getline(file, line)) {
+        if (line.empty()) {
+            continue;
+        }
+
+        std::stringstream ss(line);
+        std::string id_str, amount_str, category, date, type_str, comment;
+
+        if (std::getline(ss, id_str, ';') &&
+            std::getline(ss, amount_str, ';') &&
+            std::getline(ss, category, ';') && std::getline(ss, date, ';') &&
+            std::getline(ss, type_str, ';')) {
+
+            std::getline(ss, comment);
+
+            Transaction t;
+            t.id = std::stoi(id_str);
+            t.amount = std::stod(amount_str);
+            t.category = category;
+            t.date = date;
+
+            int type_int = std::stoi(type_str);
+            t.type = static_cast<Type>(type_int);
+
+            t.comment = comment;
+
+            tranactions.push_back(t);
+        }
+    }
+    file.close();
+    return tranactions;
+}
+
+void handleAdd(int argc, char* argv[], std::vector<Transaction>& list,
+               const std::string& filename) {}
+void handleList(const std::vector<Transaction>& transactions);
+void handleBalance(const std::vector<Transaction>& transactions);
+void handleStats(const std::vector<Transaction>& transactions);
+void printHelp();
 
 int main(int argc, char* argv[]) {
     // auto now = std::chrono::system_clock::now();
@@ -101,24 +103,25 @@ int main(int argc, char* argv[]) {
     // auto local_date =
     //     std::chrono::floor<std::chrono::days>(zoned.get_local_time());
     // std::chrono::year_month_day ymd{local_date};
-    // Transaction transaction1{1,
-    //                          1000,
-    //                          "Food",
-    //                          std::format("{:%F}", ymd),
-    //                          Type::INCOME,
-    //                          "My food at lunch"};
-    // std::vector<Transaction> transactions;
-    // transactions.emplace_back(transaction1);
-    FileManager fileMgr;
-    // fileMgr.saveToFile("tranactions.csv", transactions);
-    std::vector<Transaction> tranactions{
-        fileMgr.loadFromFile("tranactions.csv")};
-    for (const auto& transaction : tranactions) {
-        std::println(
-            "ID: {} Amount: {} Category: {} Date: {} Type: {} Comment: {}",
-            transaction.id, transaction.amount, transaction.category,
-            transaction.date, std::to_underlying(transaction.type),
-            transaction.comment);
+    if (argc < 2) {
+        printHelp();
+    }
+
+    std::string filename{"data.csv"};
+    std::vector<Transaction> transactions{loadFromFile(filename)};
+
+    std::string command{argv[1]};
+
+    if (command == "add") {
+        handleAdd(argc, argv, transactions, filename);
+    } else if (command == "list") {
+        handleList(transactions);
+    } else if (command == "balance") {
+        handleBalance(transactions);
+    } else if (command == "stats") {
+        handleStats(transactions);
+    } else {
+        std::println("Неизвестная команда. Введите 'help' для справки.");
     }
 
     return 0;
